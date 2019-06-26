e847e92d74db39b2399bc178ec22bf012e76b62a
BoHong Li <raccoon@hackmd.io>
2019-06-27 03:03:08 +0800

    feat: store node_modules

--- cmd/restore-cache.sh
+++ cmd/restore-cache.sh
@@ -4,7 +4,7 @@ set -x
 
 CACHE_FILE=npm-cache-$(md5sum package-lock.json | awk '{print $1}').tar.gz
 
-jx step unstash -u "gs://hackmd-dev-lts-29a00409-d9f7-4077-b289-4caab6b75ea0/jenkins-x/jx-node-app-npm-cache/$CACHE_FILE"
+jx step unstash -u "gs://hackmd-dev-lts-29a00409-d9f7-4077-b289-4caab6b75ea0/jenkins-x/jx-node-app-npm-cache/$CACHE_FILE" -o "$CACHE_FILE"
 
 if [[ -f $CACHE_FILE ]]; then
     tar xf "$CACHE_FILE"
